08f8b58917b451738c50c6a858d2b0f7188bda14
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-03-28 08:49:10 +0100

    Update MinesweeperGame.java

--- MinesweeperGame.java
+++ MinesweeperGame.java
@@ -4,13 +4,24 @@ import com.codegym.engine.cell.*;
 
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame(){
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
+                gameField[i][j] = new GameObject(j, i);
+                setCellColor(j, i, Color.ORANGE);
+            }
+        }
     }
 
     public static void main(String[] args) {
-        new MinesweeperGame().initialize();
+
     }
 }
